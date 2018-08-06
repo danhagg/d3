db7ac854b1843e1ff00ae837953fe0547140fa67
danhagg <danielhaggerty1976@gmail.com>
2018-08-05 19:27:49 -0500

    updates

---
+++ Principles_OOP/prinitives_references.js
@@ -0,0 +1,18 @@
+/*
+
+Primitive type
+True, False, Null, Undefined, String, Number
+
+Primitive tyoes have literal representation of their values (that arent stored in a variable)
+
+A variable holding a primitive directly contains the primitive value
+
+Despite the fact that they have methods, primitive values themselves are not objects. JavaScript makes them look like objects to provide a consistent experience in the language
+
+*/
+
+console.log(typeof "Nicholas");
+console.log(typeof undefined);
+console.log(typeof null);
+
+console.log(undefined == null); console.log(undefined === null);
